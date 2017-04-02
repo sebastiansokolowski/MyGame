a76433137ce50ceda511c5534f7ff0ce86b8450d
Sebastian Sokolowski <sebo366@gmail.com>
2017-04-02 14:23:37 +0200

    Add launcher icons, gradlew, gitignore etc.

---
+++ android/src/com/sebastian/sokolowski/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.sebastian.sokolowski.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.sebastian.sokolowski.game.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
