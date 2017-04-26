918430e8a77aa18d6c91c7cd1fd631d8ed02260b
Sebastian Sokolowski <sebo366@gmail.com>
2017-04-27 00:41:45 +0200

    Implement player move animations. Change player jump vector.

--- core/src/com/sebastian/sokolowski/game/sprites/Player.java
+++ core/src/com/sebastian/sokolowski/game/sprites/Player.java
@@ -1,5 +1,6 @@
 package com.sebastian.sokolowski.game.sprites;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -7,6 +8,7 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
 import com.sebastian.sokolowski.game.MyGdxGame;
 import com.sebastian.sokolowski.game.screens.PlayScreen;
 
@@ -15,15 +17,39 @@ import com.sebastian.sokolowski.game.screens.PlayScreen;
  */
 
 public class Player extends Sprite {
+    public enum State {CROUCHING, FALLING, JUMPING, STANDING, RUNNING}
+
+    public State currentState;
+    public State previousState;
     public World world;
     public Body body;
+
     private TextureRegion playerStand;
+    private Animation playerRun;
+    private TextureRegion playerJump;
+    private TextureRegion playerCrouching;
+
+    private float stateTimer;
+    private boolean runningRight;
 
     public Player(World world, PlayScreen playScreen) {
         super(playScreen.getTextureAtlas().findRegion("basic"));
         this.world = world;
-        definePlayer();
+        currentState = State.STANDING;
+        previousState = State.STANDING;
+        stateTimer = 0;
+        runningRight = true;
+
+        Array<TextureRegion> frames = new Array<TextureRegion>();
+        for (int i = 3; i < 7; i++) {
+            frames.add(new TextureRegion(getTexture(), i * 50, 0, 50, 50));
+        }
+        playerRun = new Animation(0.1f, frames);
+        playerJump = new TextureRegion(getTexture(), 2 * 50, 0, 50, 50);
+        playerCrouching = new TextureRegion(getTexture(), 50, 0, 50, 50);
         playerStand = new TextureRegion(getTexture(), 0, 0, 50, 50);
+
+        definePlayer();
         setBounds(0, 0, 50 / MyGdxGame.PPM, 50 / MyGdxGame.PPM);
         setRegion(playerStand);
     }
@@ -37,7 +63,7 @@ public class Player extends Sprite {
 
         FixtureDef fixtureDef = new FixtureDef();
         CircleShape circleShape = new CircleShape();
-        circleShape.setRadius(20 / MyGdxGame.PPM);
+        circleShape.setRadius(25 / MyGdxGame.PPM);
 
         fixtureDef.shape = circleShape;
 
@@ -46,5 +72,49 @@ public class Player extends Sprite {
 
     public void update(float delta) {
         setPosition(body.getPosition().x - getWidth() / 2, body.getPosition().y - getHeight() / 2);
+        setRegion(getFrame(delta));
+    }
+
+    private TextureRegion getFrame(float delta) {
+        currentState = getState();
+
+        TextureRegion textureRegion;
+        switch (currentState) {
+            case JUMPING:
+                textureRegion = playerJump;
+                break;
+            case RUNNING:
+                textureRegion = (TextureRegion) playerRun.getKeyFrame(stateTimer, true);
+                break;
+            case CROUCHING:
+                //TODO:
+            case FALLING:
+            default:
+                textureRegion = playerStand;
+        }
+
+        if ((body.getLinearVelocity().x < 0 || !runningRight) && !textureRegion.isFlipX()) {
+            textureRegion.flip(true, false);
+            runningRight = false;
+        } else if ((body.getLinearVelocity().x > 0 || runningRight) && textureRegion.isFlipX()) {
+            textureRegion.flip(true, false);
+            runningRight = true;
+        }
+
+        stateTimer = currentState == previousState ? stateTimer + delta : 0;
+        previousState = currentState;
+        return textureRegion;
+    }
+
+    private State getState() {
+        if (body.getLinearVelocity().y > 0) {
+            return State.JUMPING;
+        } else if (body.getLinearVelocity().y < 0) {
+            return State.FALLING;
+        } else if (body.getLinearVelocity().x != 0) {
+            return State.RUNNING;
+        } else {
+            return State.STANDING;
+        }
     }
 }
